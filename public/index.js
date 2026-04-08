7eaaf65c53056b7db7ccb28643b2cf1745750a83
bhoyepravin <bhoyepravin288@gmail.com>
2026-04-08 17:21:02 +0530

    images updates

--- public/index.js
+++ public/index.js
@@ -6,10 +6,35 @@ import slide_5 from "../public/herosection/slide5.jpg"
 import slide_6 from "../public/herosection/slide6.jpg"
 import slide_7 from "../public/herosection/slide7.jpg"
 
-
+import cooling_tower from "../public/products/cooling_tower1.jpg";
 import scale_products from "../public/products/scale.png";
+import corrosion_products from "../public/products/corrosion1.jpg";
+import fouling_products from "../public/products/fouling1.jpg"
+import microbiological_contaminants from "../public/products/microbiological1.jpg";
+import chiller from "../public/products/chiller1.jpg";
+import boiler_plant from "../public/products/boiler_plant.jpg";
+import descaling_products from "../public/products/descaling_products.png";
+import corrosion_boiler_water from "../public/products/boiler_water_corrosion.jpg";
 
-export { unique_aqua_logo, slide_1, slide_2, slide_4, slide_5, slide_6, slide_7, scale_products };
+import boiler_water_corrosion from "../public/products/boiler_water_corrosion.jpg";
+import scaling_water_corrosion from "../public/products/boiler_water_treatment_scaling.jpg";
+import pecialitys_chemical from "../public/products/pecialitys_chemical.jpg";
+import revers_osmosis from "../public/products/rev_osmosis.jpg";
+import sugar_factory from "../public/products/sugar_factory.jpg";
+
+import sand_filter from "../public/products/sand_filter.jpg";
+import water_softner from "../public/products/water_softner.jpg";
+import dmplant from "../public/products/dmplant.jpg";
+import ro_plant from "../public/products/ro_plant.jpg";
+import uv_disinfection from "../public/products/ultra_violet_infe.jpg";
+
+import water_kit from "../public/products/water_kit.jpg";
+import string_cartridge from "../public/products/string_cartridgeg.jpg";
+import frp_pressure_vessels from "../public/products/frp_pressure_vessels.jpg";
+
+
+
+export { unique_aqua_logo, slide_1, slide_2, slide_4, slide_5, slide_6, slide_7, cooling_tower, scale_products, corrosion_products, microbiological_contaminants, fouling_products , chiller, boiler_plant, descaling_products, corrosion_boiler_water,scaling_water_corrosion, boiler_water_corrosion ,pecialitys_chemical , revers_osmosis, sugar_factory, sand_filter, water_softner , dmplant,ro_plant, uv_disinfection, water_kit, string_cartridge, frp_pressure_vessels };
 
 import ro500Vertical from "../public/productgallery/500_lph_ss_vertical_ro.jpeg";
 import ro500SS from "@/public/productgallery/500_iph_ss_ro.jpeg";
@@ -23,5 +48,6 @@ import ro250 from "@/public/productgallery/ro_plant_250_lph.jpg";
 import ro250Vertical from "@/public/productgallery/250_lph_vertical_ro.jpeg";
 import ro2000 from "@/public/productgallery/2000_lph_ro.jpeg";
 import ro2000UV from "@/public/productgallery/2000_lph_fully_ss_ro_with_uv_tank.jpeg";
+
 
 export { ro500Vertical, ro500SS, ro500Chlorination, ro5000, roPlant5000, softener1000Auto, exportSystem, softener450, ro250, ro250Vertical, ro2000, ro2000UV };
